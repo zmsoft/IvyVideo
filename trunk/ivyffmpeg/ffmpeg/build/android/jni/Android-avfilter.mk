d23746781063acd1b39b5faa3b579a9973d0cc1e
uskee <uskee521@gmail.com>
2012-10-27 14:17:17 +0800

    Port new libs avfilter, postproc and swscale to android

---
+++ trunk/ivyffmpeg/ffmpeg/build/android/jni/Android-avfilter.mk
@@ -0,0 +1,23 @@
+ROOT_PATH := $(call my-dir)/../../..
+LOCAL_PATH := $(ROOT_PATH)/libavfilter
+EXT_PATH := $(ROOT_PATH)/..
+include $(CLEAR_VARS)
+
+include $(LOCAL_PATH)/../av.mk
+
+LOCAL_SRC_FILES := $(FFFILES)
+
+LOCAL_C_INCLUDES :=		\
+	$(LOCAL_PATH)		\
+	$(LOCAL_PATH)/..	\
+	$(EXT_PATH)/zlib
+
+LOCAL_CFLAGS += $(FFCFLAGS)
+LOCAL_CFLAGS += -include "string.h" -Dipv6mr_interface=ipv6mr_ifindex
+
+LOCAL_SHARED_LIBRARIES := libz
+LOCAL_STATIC_LIBRARIES := $(FFLIBS)
+
+LOCAL_MODULE := $(FFNAME)
+
+include $(BUILD_STATIC_LIBRARY)
